ae885457d3fe072aca6ac6ff4d192a4389e573d5
RamPaliwal <rampaliwal2002@gmail.com>
2024-03-11 17:11:00 +0530

    Added UC5 Wage Per Month

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -2,8 +2,11 @@ public class Employee_Wage{
 	public static final int wage_per_hour = 20;
         public static final int emp_part_time=1;
         public static final int emp_full_time=2;
+        public static final int total_num_of_days=20;
     	public static void main(String[] args) {
         int emp_hours = 0;
+        int total_wage=0;
+        for(int day=0;day<total_num_of_days;day++){
         int present = (int) Math.floor(Math.random()*10)%3;
         switch (present) {
             case emp_full_time:
@@ -18,6 +21,9 @@ public class Employee_Wage{
                 break;
         }
         int emp_wage = emp_hours * wage_per_hour;
-        System.out.println(emp_wage);
+        total_wage += emp_wage;
+        }
+        System.out.println(total_wage);
     }
+
 }
