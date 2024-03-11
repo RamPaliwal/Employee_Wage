7ed9621c11bd5456a7797f1e8b9603bcbb7b80de
RamPaliwal <rampaliwal2002@gmail.com>
2024-03-11 17:06:33 +0530

    Added UC4 Using Switch Case

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,21 +1,23 @@
 public class Employee_Wage{
-	public static void main(String args[]){
-		System.out.println("Welcome to Employee Wage program");
-		int wage_per_hour = 20;
-        	int emp_hours = 0;
-        	int emp_part_time=1;
-        	int emp_full_time=2;
-        	int present = (int) Math.floor(Math.random()*10)%3;
-        	if(present==emp_full_time){
-            		emp_hours = 8;
-        	}
-        	else if(present==emp_part_time){
-            		emp_hours=4;
-        	}
-        	else{
-            		emp_hours=0;
-        	}
-        	int emp_wage = emp_hours * wage_per_hour;
-        	System.out.println(emp_wage);
-	}
+	public static final int wage_per_hour = 20;
+        public static final int emp_part_time=1;
+        public static final int emp_full_time=2;
+    	public static void main(String[] args) {
+        int emp_hours = 0;
+        int present = (int) Math.floor(Math.random()*10)%3;
+        switch (present) {
+            case emp_full_time:
+                emp_hours=8;
+                break;
+
+            case emp_part_time:
+                emp_hours=4;
+        
+            default:
+                emp_hours=0;
+                break;
+        }
+        int emp_wage = emp_hours * wage_per_hour;
+        System.out.println(emp_wage);
+    }
 }
