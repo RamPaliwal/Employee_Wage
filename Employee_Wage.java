063547b1eab80a9602b58adcdda65a9f1bd21fac
RamPaliwal <rampaliwal2002@gmail.com>
2024-03-11 17:02:04 +0530

    Added UC3 Part Time Employee

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -3,9 +3,14 @@ public class Employee_Wage{
 		System.out.println("Welcome to Employee Wage program");
 		int wage_per_hour = 20;
         	int emp_hours = 0;
-        	int present = (int) Math.floor(Math.random()*10)%2;
-        	if(present==1){
+        	int emp_part_time=1;
+        	int emp_full_time=2;
+        	int present = (int) Math.floor(Math.random()*10)%3;
+        	if(present==emp_full_time){
             		emp_hours = 8;
+        	}
+        	else if(present==emp_part_time){
+            		emp_hours=4;
         	}
         	else{
             		emp_hours=0;
