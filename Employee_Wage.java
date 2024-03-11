5ecd625e45c9a5b91b5cc6dfcbc95bcfc86fb32c
RamPaliwal <rampaliwal2002@gmail.com>
2024-03-11 16:45:48 +0530

    Added UC1 EmpPresence

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,5 +1,13 @@
 public class Employee_Wage{
 	public static void main(String args[]){
 		System.out.println("Welcome to Employee Wage program");
+		int present = (int) Math.floor(Math.random()*10)%2;
+    		if(present==1){
+        		System.out.println("Employee Is Present");
+    		}
+    		else{
+    	    		System.out.println("Employee is absent");
+    		}
+
 	}
 }
