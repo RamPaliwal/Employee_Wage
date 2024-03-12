c129995f79fc58514d9155398c8f176376a0ff01
RamPaliwal <rampaliwal2002@gmail.com>
2024-03-12 11:30:59 +0530

    Added UC7 with class method

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,34 +1,36 @@
 public class Employee_Wage{
 	public static final int wage_per_hour = 20;
-    	public static final int emp_part_time=1;
-    	public static final int emp_full_time=2;
-    	public static final int total_num_of_days=20;
-    	public static final int total_hours=100;
-    	public static void main(String[] args) {
-        	int emp_hours = 0;
-        	int emp_working_hours=0;
-        	int total_wage=0;
-        	int total_work_days=0;
-        	while(emp_working_hours<total_hours && total_work_days<=total_num_of_days){
-            		total_work_days++;
-            		int present = (int) Math.floor(Math.random()*10)%3;
-        		switch (present) {
-            			case emp_full_time:
-                			emp_hours=8;
-                			break;
+	public static final int emp_part_time=1;
+	public static final int emp_full_time=2;
+	public static final int total_num_of_days=20;
+	public static final int total_hours=100;
+	public static int Calculate_Wage() {
+    	int emp_hours = 0;
+    	int emp_working_hours=0;
+    	int total_wage=0;
+    	int total_work_days=0;
+    	while(emp_working_hours<total_hours && total_work_days<=total_num_of_days){
+        		total_work_days++;
+        		int present = (int) Math.floor(Math.random()*10)%3;
+    		switch (present) {
+        			case emp_full_time:
+            			emp_hours=8;
+            			break;
 
-            			case emp_part_time:
-                			emp_hours=4;
-					break;
-        
-            			default:
-                			emp_hours=0;
-                			break;
-            		}
-            emp_working_hours += emp_hours;
-        }
-        total_wage = emp_working_hours * wage_per_hour;
-        System.out.println(total_wage);
+        			case emp_part_time:
+            			emp_hours=4;
+				break;
+    
+        			default:
+            			emp_hours=0;
+            			break;
+        		}
+        emp_working_hours += emp_hours;
     }
-
+    total_wage = emp_working_hours * wage_per_hour;
+    return total_wage;
+	}
+public static void main(String[] args) {
+	System.out.println(Employee_WageUC7.Calculate_Wage());
+	}
 }
