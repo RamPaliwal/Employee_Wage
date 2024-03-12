f5f5cfc404da9114954c3a0dab2e6bd3b2443189
RamPaliwal <rampaliwal2002@gmail.com>
2024-03-12 12:49:31 +0530

    Added UC9 to save total wage for each company

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,13 +1,23 @@
 public class Employee_Wage{
 	public static final int emp_part_time=1;
 	public static final int emp_full_time=2;
+	public final String company;
+	public final int wage_per_hour;
+	public final int total_num_of_days;
+	public final int total_hours;
+	public int total_wage;
 	
-	public static int Calculate_Wage(String name, int wage_per_hour, int total_num_of_days, int total_hours) {
+	public Employee_Wage(String company,int wage_per_hour,int total_num_of_days,int total_hours) {
+		this.company = company;
+		this.wage_per_hour = wage_per_hour;
+		this.total_num_of_days=total_num_of_days;
+		this.total_hours=total_hours;
+	}
+	public void Calculate_Wage() {
     	int emp_hours = 0;
     	int emp_working_hours=0;
-    	int total_wage=0;
     	int total_work_days=0;
-    	while(emp_working_hours<=total_hours && total_work_days<total_num_of_days){
+    	while(emp_working_hours<=total_hours && total_work_days<=total_num_of_days){
         		total_work_days++;
         		int present = (int) Math.floor(Math.random()*10)%3;
     		switch (present) {
@@ -24,12 +34,20 @@ public class Employee_Wage{
             			break;
         		}
         emp_working_hours += emp_hours;
-    		}
-    	total_wage = emp_working_hours * wage_per_hour;
-    	return total_wage;
+    	}
+   	total_wage = emp_working_hours * wage_per_hour;
 	}
 	
+	public String toString() {
+		return "Total Employee wage for company "+company+" is "+total_wage;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(Employee_Wage.Calculate_Wage("Jio",40,25,20));
+		Employee_Wage jio = new Employee_Wage("Jio",40,25,20);
+		jio.Calculate_Wage();
+		System.out.println(jio);
+		Employee_Wage dmart = new Employee_Wage("DMart",20,20,10);
+		dmart.Calculate_Wage();
+		System.out.println(dmart);
 	}
 }
