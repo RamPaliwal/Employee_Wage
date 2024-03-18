82b3ae0bb251a9928b4dd36c52e33c7ebb45e69e
RamPaliwal <rampaliwal2002@gmail.com>
2024-03-18 11:07:36 +0530

    Added UC10 for multiple companies

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,53 +1,72 @@
-public class Employee_Wage{
-	public static final int emp_part_time=1;
-	public static final int emp_full_time=2;
-	public final String company;
-	public final int wage_per_hour;
-	public final int total_num_of_days;
-	public final int total_hours;
-	public int total_wage;
-	
-	public Employee_Wage(String company,int wage_per_hour,int total_num_of_days,int total_hours) {
-		this.company = company;
-		this.wage_per_hour = wage_per_hour;
-		this.total_num_of_days=total_num_of_days;
-		this.total_hours=total_hours;
-	}
-	public void Calculate_Wage() {
-    	int emp_hours = 0;
-    	int emp_working_hours=0;
-    	int total_work_days=0;
-    	while(emp_working_hours<=total_hours && total_work_days<=total_num_of_days){
-        		total_work_days++;
-        		int present = (int) Math.floor(Math.random()*10)%3;
-    		switch (present) {
-        			case emp_full_time:
-            			emp_hours=8;
-            			break;
+import java.util.Random;
 
-        			case emp_part_time:
-            			emp_hours=4;
-				break;
-    
-        			default:
-            			emp_hours=0;
-            			break;
-        		}
-        emp_working_hours += emp_hours;
-    	}
-   	total_wage = emp_working_hours * wage_per_hour;
-	}
-	
-	public String toString() {
-		return "Total Employee wage for company "+company+" is "+total_wage;
-	}
+interface IEmployeeWage {
+    void calculateWage();
+}
 
-	public static void main(String[] args) {
-		Employee_Wage jio = new Employee_Wage("Jio",40,25,20);
-		jio.Calculate_Wage();
-		System.out.println(jio);
-		Employee_Wage dmart = new Employee_Wage("DMart",20,20,10);
-		dmart.Calculate_Wage();
-		System.out.println(dmart);
-	}
+class EmployeeWage implements IEmployeeWage {
+    private final String company;
+    private final int ratePerHr;
+    private final int maxWorkingDays;
+    private final int maxWorkinghrs;
+    private int totalEmpWage;
+
+    public EmployeeWage(String company, int ratePerHr, int maxWorkingDays, int maxWorkinghrs) {
+        this.company = company;
+        this.ratePerHr = ratePerHr;
+        this.maxWorkingDays = maxWorkingDays;
+        this.maxWorkinghrs = maxWorkinghrs;
+    }
+
+    @Override
+    public void calculateWage() {
+        int empHr = 0, totalWorkHrs = 0, totalWorkDays = 0;
+
+        while (totalWorkDays < maxWorkingDays && totalWorkHrs < maxWorkinghrs) {
+            totalWorkDays++;
+            int empPresent = new Random().nextInt(3);
+
+            switch (empPresent) {
+                case 1:
+                    empHr = 8; // Full-time
+                    break;
+                case 2:
+                    empHr = 4; // Part-time
+                    break;
+                default:
+                    empHr = 0; // Absent
+            }
+
+            totalWorkHrs += empHr;
+        }
+        totalEmpWage = totalWorkHrs * ratePerHr;
+    }
+
+    @Override
+    public String toString() {
+        return "Details of " + company + " employee\n" +
+                "-----------------------------------------------------\n" +
+                "Wage per hour: " + ratePerHr + "\n" +
+                "Maximum working days: " + maxWorkingDays + "\n" +
+                "Maximum working hours: " + maxWorkinghrs + "\n" +
+                "Total wage for a month of " + company + " employee is " + totalEmpWage + "\n";
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Employee Wage Computation Program....");
+
+        IEmployeeWage[] companies = new IEmployeeWage[3];
+        companies[0] = new EmployeeWage("Google", 20, 20, 100);
+        companies[1] = new EmployeeWage("Microsoft", 18, 20, 120);
+        companies[2] = new EmployeeWage("Amazon", 24, 22, 130);
+
+        for(int i = 0; i < companies.length; i++)
+        {
+            IEmployeeWage company = companies[i];
+            company.calculateWage();
+            System.out.println(company);
+        }
+
+    }
 }
